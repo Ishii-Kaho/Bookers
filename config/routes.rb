5bff0f76febd4cea7242ad69d54ef8c0bf07aa8d
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-08 05:38:31 +0000

    [finish]top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # booksのindex,show,new,editのルーティングを自動で行う
+  resource :books
+  # homesのindex,show,new,editのルーティングを自動で行う
+  resource :homes
+  # topページのルーティング
+  get 'top' => 'homes#top'
+  # indexページのルーティング
+  get 'index' => 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
