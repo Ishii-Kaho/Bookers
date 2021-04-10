82f85b9e6ed883e550a8c40fa04732a3a7f78559
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-10 07:13:23 +0000

    [finish]index-page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,33 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    # viewに渡すためのインスタンス変数に空のモデルオブジェクトを生成する
+    # @book = Book.new
+  end
+
+  def create
+    # データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # logger.debug("標準出力とログファイルに記録される")
+    # 詳細ページへリダイレクト
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # booksのindex,show,new,editのルーティングを自動で行う
-  resource :books
+  resources :books
   # homesのindex,show,new,editのルーティングを自動で行う
-  resource :homes
+  resources :homes
   # topページのルーティング
   get 'top' => 'homes#top'
-  # indexページのルーティング
-  get 'index' => 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
