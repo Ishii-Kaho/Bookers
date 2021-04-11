f3c0b48dc29ad558d7c2b64a8c759416f446cce2
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-11 06:19:51 +0000

    [finish]update-destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,25 +4,25 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @books = Book.order(id: "DESC")
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    # viewに渡すためのインスタンス変数に空のモデルオブジェクトを生成する
-    # @book = Book.new
-  end
-
   def create
     # データを新規登録するためのインスタンス作成
     book = Book.new(book_params)
     # データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if book.save
     # logger.debug("標準出力とログファイルに記録される")
     # 詳細ページへリダイレクト
-    redirect_to book_path(book.id)
+     redirect_to book_path(book.id)
+    else
+      rander "new"
+    end
   end
 
   def edit
@@ -30,9 +30,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book), notice: "You have updated book successfully."
+      else
+        render "edit"
+      end
   end
 
   def destroy
@@ -43,6 +46,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
