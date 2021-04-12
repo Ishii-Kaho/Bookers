2a78a478313d461521e09fc0bfa41b7eebd1ce53
Ishii-Kaho <kh141smallbear@gmail.com>
2021-04-12 05:58:59 +0000

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # topページを/URLに変更する
+  root to: 'homes#top'
   # booksのindex,show,new,editのルーティングを自動で行う
   resources :books
   # homesのindex,show,new,editのルーティングを自動で行う
